1d3e636eaa673539cb60039486d2eb0cacb0de12
EstebanGomezVal <157132736+EstebanGomezVal@users.noreply.github.com>
2025-11-11 21:35:47 -0600

    feat: Docker

--- app/api/api.py
+++ app/api/api.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel
 from mlflow import MlflowClient
 from dotenv import load_dotenv
 import os
+import pandas as pd
 
 load_dotenv(override=True)  # Carga las variables del archivo .env
 
@@ -15,7 +16,7 @@ EXPERIMENT_NAME = "/Users/estebangmzv@gmail.com/nyc-taxi-experiment-prefect"
 
 run_ = mlflow.search_runs(order_by=['metrics.rmse ASC'],
                           output_format="list",
-                          experiment_names=[EXPERIMENT_NAME]
+                          experiment_names=EXPERIMENT_NAME
                           )[0]
 
 run_id = run_.info.run_id
@@ -31,7 +32,7 @@ client.download_artifacts(
 with open("preprocessor/preprocessor.b", "rb") as f_in:
     dv = pickle.load(f_in)
 
-model_name = "workspace.default.nyc-taxi-model"
+model_name = "nyc-taxi-model"
 alias = "champion"
 
 model_uri = f"models:/{model_name}@{alias}"
@@ -40,23 +41,29 @@ champion_model = mlflow.pyfunc.load_model(
     model_uri=model_uri
 )
 
-with open("preprocessor/preprocessor.b", "rb") as f_in:
-    dv = pickle.load(f_in)
-
 def preprocess(input_data):
 
     input_dict = {
         'PU_DO': input_data.PULocationID + "_" + input_data.DOLocationID,
         'trip_distance': input_data.trip_distance,
     }
+    X = dv.transform([input_dict])
 
-    return dv.transform(input_dict)
+    # Names depend on sklearn version
+    try:
+        cols = dv.get_feature_names_out()
+    except AttributeError:
+        cols = dv.get_feature_names()
+
+    X_df = pd.DataFrame(X.toarray(), columns=cols)
+
+    return X_df
 
 def predict(input_data):
 
     X_val = preprocess(input_data)
 
-    return "4"#champion_model.predict(X_val)
+    return champion_model.predict(X_val)
 
 app = FastAPI()
 
